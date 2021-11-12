5790a520768a6a98289e5cdba2b36f11c4f9d0d1
Naomi Mango <naomi.mango@gmail.com>
2021-11-12 15:15:47 +0100

    [NEW] Exercice 22

---
+++ Exercice_22/exercice22.js
@@ -0,0 +1,18 @@
+/****************************************** EXERCICE 22 *******************************************/
+
+function estBissextile(annee) {
+  return ((annee % 4 === 0 && annee % 100 === 0) || annee % 400 === 0);
+}
+
+function affichageAnneeBissextile(annee) {
+  var msg = annee + " est une année ";
+  if (!estBissextile(annee)) {
+    msg += "non ";
+  }
+  msg += "bissextile";
+  console.log(msg);
+}
+
+affichageAnneeBissextile(1900);
+affichageAnneeBissextile(2000);
+affichageAnneeBissextile(2020);
