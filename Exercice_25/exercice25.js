f92b843fbc654b61bc529ee651d3d1a7a7180be3
Naomi Mango <naomi.mango@gmail.com>
2021-11-12 16:43:20 +0100

    [MAJ] Exercice 25

--- Exercice_25/exercice25.js
+++ Exercice_25/exercice25.js
@@ -4,12 +4,26 @@ var readline = require("readline-sync");
 
 var taille = readline.questionInt("Quelle taille voulez-vous ? ");
 
-var ligne = "***************";
-console.log(ligne);
+var ligne = "";
 
-for (var cpt = 1; cpt <= taille - 2; cpt++) {
-  var hauteur = "**           **";
-  console.log(hauteur);
+for (var cpt1 = 1; cpt1 <= taille; cpt1++) {
+  ligne = ligne + "**";
 }
 
-console.log(ligne);
+var hauteur = "";
+
+for (var cpt2 = 1; cpt2 <= taille; cpt2++) {
+  if (cpt2 === 1 || cpt2 === taille) { // bordure de gauche || bordure de droite
+    hauteur = hauteur + "**";
+  } else {
+    hauteur = hauteur + "  "; // intérieur du carré
+  }
+}
+
+for (var cpt0 = 1; cpt0 <= taille; cpt0++) {
+  if (cpt0 === 1 || cpt0 === taille) { // bordure du haut || bordure du bas
+    console.log(ligne);
+  } else {
+    console.log(hauteur);
+  }
+}
